1d193f0557e85f34aa9379f1cb8004015e4f287f
SirichaiKhomleart <32129292+SirichaiKhomleart@users.noreply.github.com>
2018-02-17 22:52:43 +0700

    Add more 4 pages

---
+++ recordInfo.php
@@ -0,0 +1,104 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Record Information</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+</head>
+<body class="subpage">
+
+<!-- Header -->
+<header id="header" >
+    <div class="logo"><a href="index.html">Hospital Collaboration Resource <i class="fas fa-hospital"></i></a></div>
+    <a href="#menu" class="toggle"><span>Menu</span></a>
+</header>
+
+<!-- Nav -->
+<nav id="menu">
+    <ul class="links">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="">Logout</a></li>
+    </ul>
+</nav>
+
+<!-- One -->
+<section id="one" class="wrapper style2">
+    <div class="inner">
+        <div class="box">
+            <div class="image fit">
+                <img src="images/pic01.jpg" alt="" />
+            </div>
+            <div class="content">
+                <header class="align-center">
+                    <h2>Record Name</h2>
+                    <p>Doctor Name, Hospital Name</p>
+                </header>
+                <hr>
+                <h3>Patient Information</h3>
+                <dl>
+                    <dt><b>Patient Name</b></dt>
+                    <dd>
+                        <p>Patient Name</p>
+                    </dd>
+                    <dt><b>Gender</b></dt>
+                    <dd>
+                        <p>Male</p>
+                    </dd>
+                    <dt><b>Weight and Height</b></dt>
+                    <dd>
+                        <p>65 kg, 175 km</p>
+                    </dd>
+                </dl>
+                <hr>
+                <h3>Record Information</h3>
+                <dl>
+                    <dt><b>Record Date</b></dt>
+                    <dd>
+                        <p>March 24, 2017</p>
+                    </dd>
+                    <dt><b>Description</b></dt>
+                    <dd>
+                        <p>kvhksdfhgiukrshfivhdgkhiglhvio'hgihdfg'sidhfgdjfgshdfghdifb</p>
+                    </dd>
+                    <dt><b>Prescription</b></dt>
+                    <dd>
+                        <p>gshjnjghifudkbvnijhsgnfjjshgfnjghdn</p>
+                    </dd>
+                </dl>
+
+                <hr>
+                <ul class="actions" align="center">
+                    <li><a href="#" class="button icon fa-download">Add more Record</a></li>
+                    <li><a href="#" class="button alt icon fa-search">Compare Others</a></li>
+                </ul>
+
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<!-- Four -->
+<section id="four" class="wrapper style3">
+    <div class="inner">
+
+        <header class="align-center">
+            <h2>Declaration of Geneva</h2>
+            <p>THE HEALTH AND WELL-BEING OF MY PATIENT will be my first consideration</p>
+        </header>
+
+    </div>
+</section>
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.scrolly.min.js"></script>
+<script src="assets/js/jquery.scrollex.min.js"></script>
+<script src="assets/js/skel.min.js"></script>
+<script src="assets/js/util.js"></script>
+<script src="assets/js/main.js"></script>
+
+</body>
+</html>
---
+++ search.php
@@ -0,0 +1,117 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Search Information</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+</head>
+<body class="subpage">
+
+<!-- Header -->
+<header id="header" >
+    <div class="logo"><a href="index.html">Hospital Collaboration Resource <i class="fas fa-hospital"></i></a></div>
+    <a href="#menu" class="toggle"><span>Menu</span></a>
+</header>
+
+<!-- Nav -->
+<nav id="menu">
+    <ul class="links">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="">Logout</a></li>
+    </ul>
+</nav>
+
+<!-- One -->
+<section id="one" class="wrapper style2">
+    <div class="inner">
+        <div class="box">
+            <div class="content">
+                <header class="align-center">
+                    <h2>Search Result</h2>
+                    <p>Sorted by machine learning for the most similar case.</p>
+                </header>
+                <hr>
+                <form method="post" action="#" class="alt">
+                    <div class="row uniform">
+                        <div class="9u 12u$(small)">
+                            <input type="text" name="query" id="query" value="" placeholder="Keyword" />
+                        </div>
+                        <div class="3u$ 12u$(small)">
+                            <input type="submit" value="Filter" class="fit" />
+                        </div>
+                    </div>
+                </form>
+                <hr>
+
+                <h3>Medical Record</h3>
+                <div class="table-wrapper" align="center">
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Patient ID</th>
+                            <th>Patient Name</th>
+                            <th>Gender</th>
+                            <th>Last Record</th>
+                            <th></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>1234567890</td>
+                            <td><b>Mr.Pasin Jiratthiticheep</b></td>
+                            <td>Male</td>
+                            <td>May 16, 2015</td>
+                            <td>
+                                <li><a href="#" class="button special">Details</a></li>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>1234567890</td>
+                            <td><b>Mr.Pasin Jiratthiticheep</b></td>
+                            <td>Male</td>
+                            <td>May 16, 2015</td>
+                            <td>
+                                <li><a href="#" class="button special">Details</a></li>
+                            </td>
+                        </tr>
+                        </tbody>
+                        <tfoot>
+                        <tr>
+                            <td colspan="6"></td>
+                        </tr>
+                        </tfoot>
+                    </table>
+                </div>
+                
+
+
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<!-- Four -->
+<section id="four" class="wrapper style3">
+    <div class="inner">
+
+        <header class="align-center">
+            <h2>Declaration of Geneva</h2>
+            <p>THE HEALTH AND WELL-BEING OF MY PATIENT will be my first consideration</p>
+        </header>
+
+    </div>
+</section>
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.scrolly.min.js"></script>
+<script src="assets/js/jquery.scrollex.min.js"></script>
+<script src="assets/js/skel.min.js"></script>
+<script src="assets/js/util.js"></script>
+<script src="assets/js/main.js"></script>
+
+</body>
+</html>
