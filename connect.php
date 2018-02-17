a5707a0937f0b84640bf201f883f54cbdb83620d
SirichaiKhomleart <32129292+SirichaiKhomleart@users.noreply.github.com>
2018-02-18 05:58:36 +0700

    edit connect

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $connectionString = getenv("MYSQLCONNSTR_localdb");
 $varsString = str_replace(";", "&", $connectionString);
 parse_str($varsString);
-$conn = new mysqli($Data_Source, $User_Id, $Password, $Database);
+$conn = new mysqli($Data_Source, $User_Id, $Password, 'health');
 if($conn->connect_errno){
     echo $conn->connect_errno.": ".$conn->connect_error;
 }
