dcb3453be7f501b3866bbe655f2ccd8c6f3903d6
SirichaiKhomleart <32129292+SirichaiKhomleart@users.noreply.github.com>
2018-02-17 21:23:37 +0700

    Add patientInfo.php

---
+++ patientInfo.php
@@ -0,0 +1,133 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Patient Information</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+</head>
+<body class="subpage">
+
+<!-- Header -->
+<header id="header">
+    <div class="logo"><a href="index.html">Transitive <span>by TEMPLATED</span></a></div>
+    <a href="#menu" class="toggle"><span>Menu</span></a>
+</header>
+
+<!-- Nav -->
+<nav id="menu">
+    <ul class="links">
+        <li><a href="index.html">Home</a></li>
+        <li><a href=#>Log Out</a></li>
+    </ul>
+</nav>
+
+<!-- One -->
+<section id="one" class="wrapper style2">
+    <div class="inner">
+        <div class="box">
+            <div class="image fit">
+                <img src="images/pic01.jpg" alt="" />
+            </div>
+            <div class="content">
+                <header class="align-center">
+                    <h2>Patient Name</h2>
+                    <p>1234567890</p>
+                </header>
+                <hr />
+                <h3>Patient Information</h3>
+                <dl>
+                    <dt><b>Name</b></dt>
+                    <dd>
+                        <p>Patient Name</p>
+                    </dd>
+                    <dt><b>Gender</b></dt>
+                    <dd>
+                        <p>Male</p>
+                    </dd>
+                    <dt><b>Birth Date</b></dt>
+                    <dd>
+                        <p>Male</p>
+                    </dd>
+                    <dt><b>Address</b></dt>
+                    <dd>
+                        <p>Male</p>
+                    </dd>
+                </dl>
+                <hr>
+                        <h3>Medical Record</h3>
+                        <div class="table-wrapper" align="center">
+                            <table>
+                                <thead>
+                                <tr>
+                                    <th>Date</th>
+                                    <th>Record Name</th>
+                                    <th>Doctor Name</th>
+                                    <th>Hospital Name</th>
+                                    <th>Prescription</th>
+                                    <th></th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>February 20, 2018</td>
+                                    <td><b>Heart Broken.</b></td>
+                                    <td>Dr.Nuttapol Saiboonreng</td>
+                                    <td>Thammasat Hospital</td>
+                                    <td>No</td>
+                                    <td>
+                                        <li><a href="#" class="button special">Details</a></li>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>February 20, 2018</td>
+                                    <td><b>Heart Broken.</b></td>
+                                    <td>Dr.Nuttapol Saiboonreng</td>
+                                    <td>Thammasat Hospital</td>
+                                    <td>No</td>
+                                    <td>
+                                        <li><a href="#" class="button special">Details</a></li>
+                                    </td>
+                                </tr>
+                                </tbody>
+                                <tfoot>
+                                <tr>
+                                    <td colspan="6"></td>
+                                </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                <hr>
+                <ul class="actions" align="center">
+                    <li><a href="#" class="button icon fa-download">Add more Record</a></li>
+                    <li><a href="#" class="button alt icon fa-search">Compare Others</a></li>
+                </ul>
+
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<!-- Four -->
+<section id="four" class="wrapper style3">
+    <div class="inner">
+
+        <header class="align-center">
+            <h2>Declaration of Geneva</h2>
+            <p>THE HEALTH AND WELL-BEING OF MY PATIENT will be my first consideration</p>
+        </header>
+
+    </div>
+</section>
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.scrolly.min.js"></script>
+<script src="assets/js/jquery.scrollex.min.js"></script>
+<script src="assets/js/skel.min.js"></script>
+<script src="assets/js/util.js"></script>
+<script src="assets/js/main.js"></script>
+
+</body>
+</html>
